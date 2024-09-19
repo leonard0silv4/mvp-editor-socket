2bbe4ae66dd64854ef7e350601c3595325ea10bc
Leonardo Silva <lorsilva.fcamara@rd.com.br>
2024-09-19 20:13:35 -0300

    heroku

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
